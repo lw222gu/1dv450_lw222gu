f7095ba1117c1156e55561aaa8e80323cddf11d6
Lisa Westlund <lw222gu@student.lnu.se>
2016-01-27 21:48:42 +0000

    User can log in

--- apiKeyRegistration/app/controllers/session_controller.rb
+++ apiKeyRegistration/app/controllers/session_controller.rb
@@ -1,4 +1,20 @@
 class SessionController < ApplicationController
+    
+    def index
+    end
+    
     def login
+        u = User.find_by_username(params[:username])
+        if u && u.authenticate(params[:password])
+            session[:userid] = u.id
+            redirect_to apikey_path
+        else
+            flash[:notice] = 'Fel vid inloggning.'
+            redirect_to root_path
+        end
+    end
+    
+    def logout
+        
     end
 end
