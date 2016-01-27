a98efd53aa384f0d91fa924f89c83d3a9ca87505
Lisa Westlund <lw222gu@student.lnu.se>
2016-01-27 22:12:10 +0000

    User can not access logged in page without logging in first

--- apiKeyRegistration/app/controllers/apikeys_controller.rb
+++ apiKeyRegistration/app/controllers/apikeys_controller.rb
@@ -1,4 +1,10 @@
 class ApikeysController < ApplicationController
+    
+    #Innan någon av dessa metoder körs, så vill jag kontrollera att användaren är inloggad, via require_login
+    before_action :require_login
+    
     def show
+        
     end
+    
 end
--- apiKeyRegistration/app/controllers/application_controller.rb
+++ apiKeyRegistration/app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  private
+  # helpern gör att även vyn kommer åt current_user metoden.  
+  helper_method :current_user
+  
+  def current_user
+    # ||= kör koden om den inte är satt. Annars ignoreras koden nedan.
+    @current_user ||= User.find(session[:userid]) if session[:userid]
+  end
+  
+  def require_login
+    # Om current_user returnerar nil redirectas användaren till startsidan.
+    if current_user.nil? then
+      redirect_to root_path
+    end
+  end
+  
 end
