e806a8b359f3f9bb07e723e74d83922a103db874
Lisa Westlund <lw222gu@student.lnu.se>
2016-02-04 17:19:53 +0000

    Added seeds

--- apiKeyRegistration/app/controllers/apikeys_controller.rb
+++ apiKeyRegistration/app/controllers/apikeys_controller.rb
@@ -1,7 +1,7 @@
 class ApikeysController < ApplicationController
     
     #Innan någon av dessa metoder körs, så vill jag kontrollera att användaren är inloggad, via require_login
-    before_action :require_login
+    before_action :require_login, only: [:show]
     
     def show
         @user = User.find(session[:userid])
--- apiKeyRegistration/db/seeds.rb
+++ apiKeyRegistration/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create
+admin.username = 'admin'
+admin.password = 'pass123'
+admin.password_confirmation = 'pass123'
+admin.admin = true
+admin.save
+
+u = User.create
+u.username = 'user'
+u.password = '123'
+u.password_confirmation = '123'
+u.save
+
+c = Client.create
+c.name = 'En applikation'
+c.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+c.url = 'www.test.se'
+
+charset = Array('A'..'Z') + Array('a'..'z')
+random = Array.new(10) { charset.sample }.join
+c.key = DateTime.now.strftime('%s') + random
+
+c.user_id = 2
+c.save
