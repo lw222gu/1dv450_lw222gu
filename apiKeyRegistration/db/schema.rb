1e2ca7903c645da7af20a9d189c065f04e40c1fb
Lisa Westlund <lw222gu@student.lnu.se>
2016-02-04 09:14:52 +0000

    Added admin field in user table and re-migrated db

--- apiKeyRegistration/db/schema.rb
+++ apiKeyRegistration/db/schema.rb
@@ -26,10 +26,11 @@ ActiveRecord::Schema.define(version: 20160128193410) do
   add_index "clients", ["user_id"], name: "index_clients_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",        null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "username",                        null: false
+    t.string   "password_digest",                 null: false
+    t.boolean  "admin",           default: false, null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
 end
