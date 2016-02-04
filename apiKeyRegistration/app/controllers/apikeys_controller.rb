7aa1a7df231c73649f985e92660894439b54bf0b
Lisa Westlund <lw222gu@student.lnu.se>
2016-02-04 22:14:57 +0000

    Removed unneccessary divs

--- apiKeyRegistration/app/controllers/apikeys_controller.rb
+++ apiKeyRegistration/app/controllers/apikeys_controller.rb
@@ -1,6 +1,5 @@
 class ApikeysController < ApplicationController
     
-    #Innan någon av dessa metoder körs, så vill jag kontrollera att användaren är inloggad, via require_login
     before_action :require_login, only: [:show]
     
     def show
