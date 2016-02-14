eb23c8d89e3349a5b24b109e1bc8efac8207def8
Lisa Westlund <lw222gu@student.lnu.se>
2016-02-14 11:51:13 +0000

    Fixed issue about still logged in after logged out

--- apiKeyRegistration/app/controllers/application_controller.rb
+++ apiKeyRegistration/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :no_cache
   
   private
   # makes sure that current_user is available from views  
@@ -23,6 +24,12 @@ class ApplicationController < ActionController::Base
       flash[:danger] = 'Du har inte rättigheter att visa sidan du efterfrågade.'
       redirect_to apikey_path
     end
+  end
+  
+  def no_cache
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
   
 end
